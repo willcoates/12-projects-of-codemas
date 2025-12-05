b2450d0a75f6a3d81c26ece6ae4c0f9771f00996
William Coates <will.t.coates@gmail.com>
2025-12-05 14:31:30 +0000

    day 3 - debounce calls when using irqs

--- day3/06_test_irq.py
+++ day3/06_test_irq.py
@@ -13,7 +13,22 @@ button1 = Pin(13, Pin.IN, Pin.PULL_DOWN)
 button2 = Pin(8, Pin.IN, Pin.PULL_DOWN)
 button3 = Pin(3, Pin.IN, Pin.PULL_DOWN)
 
+def debounce(period_ms = 200):
+    def decorator(fn):
+        last_invoke = time.ticks_add(time.ticks_ms(), -period_ms)
+        
+        def wrapper(*args, **kwargs):
+            nonlocal last_invoke
+            now = time.ticks_ms()
+            if time.ticks_diff(now, last_invoke) > period_ms:
+                last_invoke = now
+                fn(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@debounce()
 def on_button1_press(pin):
     red.toggle()
 
+red.value(0)
 button1.irq(on_button1_press, trigger=Pin.IRQ_RISING)
